007c0f83a9c4aa8dc1f7fb9e9b644735ae7b893d
livi1776 <36557047+livi1776@users.noreply.github.com>
2021-06-22 14:32:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+import { StyleSheet, Text, View, TextInput, Image, ImageBackground } from "react-native";
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./assets/wheel.png")}
+          resizeMode="cover"
+          style={styles.backdrop}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Capital One Ferris Wheel in the National Harbor, D.C.
+              </Text>
+            </View>
+              <Text style={styles.mainText}>Let your adventure begin</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const baseFontSize = 16;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", paddingTop: 30 },
+  backdrop: { flex: 1, flexDirection: "column",justifyContent: "center"  },
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: "#000000",
+    opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30
+  },
+  zipContainer: {
+    height: baseFontSize + 10,
+    borderBottomColor: "#DDDDDD",
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: { flex: 1, flexBasis: 1, width: 50, height: baseFontSize },
+  mainText: { fontSize: baseFontSize, color: "#FFFFFF" }
+});
+
+export default App;
